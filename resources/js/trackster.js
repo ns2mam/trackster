49b731b6fc8fc7d1cd037a911f7dafed599e8918
ns2mam <36864171+ns2mam@users.noreply.github.com>
2018-05-06 16:53:04 +0100

    Add for loop to JavaScript method Trackster.renderTracks.

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -12,7 +12,29 @@ $("#search-button").click(function() {
 const API_KEY = "1d1f6112df559817f5b57b4a5f8a6356";
 
 Trackster.renderTracks = function(tracks) {
+  var $songList = $("#song-list");
 
+  $songList.empty();
+
+  for (var songIndex = 0; songIndex < tracks.length; songIndex++) {
+    var thisSong = tracks[songIndex];
+    var songRowHtml =
+      "<div class="row song">"+
+        "<div class="col-xs-1 col-xs-offset-1 play-button">"+
+          "<a href='https://youtu.be/eI_O5_tJ1hA' target="_blank">"+
+            "<i class="fa fa-play-circle-o fa-2x"></i>"+
+          "</a>"+
+        "</div>"+
+        "<div class="col-xs-1">2</div>"+
+        "<div class="col-xs-3">She's So Groovy</div>"+
+        "<div class="col-xs-2">The Funky Monkeys</div>"+
+        "<div class="col-xs-2">Primate Life</div>"+
+        "<div class="col-xs-1">724563</div>"+
+        "<div class="col-xs-1">3:15</div>"+
+      "</div>";
+
+    $songList.append(songRowHtml);
+  }
 };
 
 /*
